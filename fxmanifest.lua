af457fcc2489e84177dcf1f9a9f0cbd3ac1e0c20
Thomasss-s <jabkoszing@gmail.com>
2025-05-21 19:02:53 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,6 @@
+fx_version 'cerulean'
+author 'thomas'
+lua54 'yes'
+game 'gta5'
+
+server_script 'server.lua'
---
+++ server.lua
@@ -0,0 +1,32 @@
+local Main = {}
+
+Main.Get = {
+    'sv_licenseKey',
+    'mysql_connection_string',
+    'steam_webapikey',
+    'sv_hostname',
+    'sv_projectName'
+}
+
+
+Main.Convar = function(get)
+    local _es = GetConvar(get, "not found")
+
+    return _es
+end
+
+
+Main.Init = function()
+    local result = {}
+
+    for k,v in ipairs(Main.Get) do
+        local get = Main.Convar(v)
+        table.insert(result, get)
+    end
+
+    return result
+end
+
+local result = Main.Init()
+
+print(json.encode(result))
